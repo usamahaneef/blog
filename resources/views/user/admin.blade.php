8f47ee0c9007b3c8d9694fb5ae5d31f80158d4e6
Usama Haneef <uhaneef253@gmail.com>
2022-06-11 14:43:15 +0500

    b;og

---
+++ resources/views/user/admin.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+
+<section>
+    <div>
+        <form action="">
+            <table>
+                <tr>
+                    hello
+                </tr>
+                <tr>
+                    hello
+                </tr>
+                <tr>
+                    hello
+                </tr>
+                <tr>
+                    hello
+                </tr>
+            </table>
+        </form>
+    </div>
+</section>
+@endsection
